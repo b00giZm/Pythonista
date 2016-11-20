f418c8ae6fa3122e2d4dbe60e09bd523978fae40
Pascal Cremer <b00gizm@gmail.com>
2016-11-20 16:13:26 +0100

    Add read functionality

--- update_repo.py
+++ update_repo.py
@@ -1,31 +1,53 @@
 from collections import OrderedDict
 import editor
 import re
+import sys
 from urllib.parse import urlencode, quote
 import webbrowser
 
 key = "D9NB4MSC6J"
+
+def get_path():
+	regex= r"\/Documents\/(.+)$"
+
+	path = editor.get_path()
+	matches = re.findall(regex, path)
+	
+	return matches[0]
+
+def get_write_url():
+	params = OrderedDict()
+	
+	params["key"]       = key
+	params["x-success"] = "pythonista://"
+	params["repo"]      = "Pythonista"
+	params["path"]      = get_path()
+	params["text"]      = editor.get_text()
+	params["askcommit"] = 1
+
+	return "working-copy://x-callback-url/write/?" + urlencode(params, quote_via=quote)
+	
+def get_read_url():
+	params = OrderedDict()
+	
+	params["key"]       = key
+	params["repo"]      = "Pythonista"
+	params["path"]      = get_path()
+	params["command"]   = "pull"
+	params["command"]   = "read"
+	params["x-success"] = "pythonista://update_script?action=run&argv="
+
+	return "working-copy://x-callback-url/chain/?" + urlencode(params, quote_via=quote)
 
 def main():
 	if key == "":
 		print("You need to fill out key with value from Working Copy settings.")
 		quit()
 		
-	regex = r"\/Documents\/(.+)$"
-
-	path = editor.get_path()
-	matches = re.findall(regex, path)
+	url = get_write_url()
+	if len(sys.argv) > 1 and sys.argv[1] == "read":
+		url = get_read_url()
 	
-	params = OrderedDict()
-	
-	params["key"]       = key
-	params["x-success"] = "pythonista://"
-	params["repo"]      = "Pythonista"
-	params["path"]      = matches[0]
-	params["text"]      = editor.get_text()
-	params["askcommit"] = 1
-
-	url = "working-copy://x-callback-url/write/?" + urlencode(params, quote_via=quote)
 	webbrowser.open(url)
 
 if __name__ == '__main__':
